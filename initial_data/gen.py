74f35cc9ccb8de7e25ef4fce8ec81c44e235f5f8
eizoassik <eizoassik@gmail.com>
2014-04-24 09:29:22 +0800

    重新设计模板格式

--- initial_data/gen.py
+++ initial_data/gen.py
@@ -1,32 +1,65 @@
-#/bin/python
+#encoding=utf-8
+# 这个简单的脚本支持Python2.7/Python3.3
+# 定义转换规则
+TRANS_RULE = [(int, lambda x: str(x)),
+              (float, lambda x: str(x)),
+              ('None', lambda x: 'NULL'),
+              (str, lambda x: "'%s'" % x.lstrip().rstrip()),
+              (None, lambda x: 'NULL')]
 
-FILENAME = "tables.template"
 
-
+# 解释转换规则
 def to_sql_value(term):
-    try:
-        v = float(term)
-        return str(v)
-    except ValueError:
-        pass
-    try:
-        v = str(term).lstrip().rstrip()
-        return "'%s'" % v
-    except Exception:
-        return 'NULL'
+    for schema, transformer in TRANS_RULE:
+        if isinstance(schema, type):
+            try:
+                v = transformer(schema(term))
+                return v
+            except ValueError:
+                pass
+        elif schema is not None:
+            if term.lstrip().rstrip() == schema:
+                return transformer(term)
+        else:
+            return transformer(term)
 
+
+def clear_whitespace(s):
+    return s.lstrip().rstrip()
+
+
+def translate(file, name):
+    rows = file.readlines()
+    src = []
+    if len(rows) < 1:
+        return []
+    title = ', '.join([clear_whitespace(t) for t in rows[0].split('|')])
+    template = "INSERT INTO %s(%s) VALUES (%s);\n"
+    for row in rows[1:]:
+        if row.startswith('--'):
+            continue
+        values = ', '.join([to_sql_value(t) for t in row.split('|')])
+        src.append(template % (name, title, values))
+    return src
+
+
+def translate_seq(file):
+    for line in sequence:
+        columns = line.split()
+        name = columns[0]
+        with open(name + '.rows') as file:
+            src = translate(file, name)
+            with open(name + '.sql', 'w') as target:
+                target.writelines(src)
+
+
+import sys
 
 if __name__ == '__main__':
-    with open(FILENAME) as template:
-        for line in template:
-            columns = line.split()
-            with open(columns[0] + ".rows") as rows:
-                src = "INSERT INTO %s(%s) VALUES (%s);\n"
-                table_name = columns[0]
-                src_filename = table_name + "_rows.sql"
-                row_names = ', '.join(columns[1:])
-                with open(src_filename, "w") as src_file:
-                    for row in rows:
-                        row_values = ', '.join(to_sql_value(value) for value in row.split('|'))
-                        final_line = src % (table_name, row_names, row_values)
-                        src_file.write(final_line)
+    SEQUENCE = []
+    if len(sys.argv) >= 2:
+        SEQUENCE = sys.argv[1:]
+    for filename in SEQUENCE:
+        with open(filename) as sequence:
+            translate_seq(sequence)
+
